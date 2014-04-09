94a8b3274b3069d18349f1665f609b2020c8e33f
Antoine Pultier <antoine.pultier@sintef.no>
2014-04-09 11:57:13 +0200

    Fetch url support, like a proxy

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var express = require('express'),
 	formidable = require('formidable'),
 	mime = require('mime'),
 	fs = require('fs'),
+	url = require('url'),
+	http = require('http'),
+	https = require('https'),
+	crypto = require('crypto'),
+	temp = require('temp'),
 	gm = require('gm'),
 	Nedb = require('nedb');
 
 var filesDb = new Nedb({filename: 'files.db', autoload:true}),
 	picturesSizeDb = new Nedb({filename: 'picturesSizes.db', autoload:true});
 
+filesDb.ensureIndex({ fieldName: 'url', unique: true, sparse: true });
+
 var app = express();
 
+app.use(express.compress());
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
@@ -104,43 +112,45 @@ app.get('/thumbnail/:path', function(req, res) {
 
 app.post('/upload', function(req, res) {
 	var form = new formidable.IncomingForm();
-	form.uploadDir = "./uploads";
+	form.uploadDir = __dirname + "/uploads";
 	form.keepExtensions = true;
 	form.hash = 'sha1';
 
 	form.parse(req, function(err, fields, files) {
-		if (err || !files || !files.file) {
+		if (err || !files) {
 			console.log("File upload error", err);
 			res.send(500, "Sorry, file upload error");
 			return;
 		}
 
-		var f = files.file;
+		for (var key in files) {
+			var f = files[key];
 
-		var extension = mime.extension(f.type);
+			var extension = mime.extension(f.type);
 
-		var path = "./uploads/"+f.hash+"."+extension;
+			var path = __dirname+"/uploads/"+f.hash+"."+extension;
 
-		fs.exists(path, function(exists) {
-			if (exists) {
-				fs.unlink(f.path);
-				res.send({name: f.name, status: 'exists'});
-			} else {
+			fs.exists(path, function(exists) {
+				if (exists) {
+					fs.unlink(f.path);
+					res.send({name: f.name, status: 'exists'});
+				} else {
 
-				fs.rename(f.path, path, function() {
-					filesDb.insert({
-						size: f.size,
-						hash: f.hash,
-						extension: extension,
-						type: f.type,
-						name: f.name,
-						mtime: f.lastModifiedDate
+					fs.rename(f.path, path, function() {
+						filesDb.insert({
+							size: f.size,
+							hash: f.hash,
+							extension: extension,
+							type: f.type,
+							name: f.name,
+							mtime: f.lastModifiedDate
+						});
+				
+						res.send({name: f.name, status: 'ok'});
 					});
-			
-					res.send({name: f.name, status: 'ok'});
-				});
-			}
-		});
+				}
+			});
+		};
 
 
 	})
@@ -181,7 +191,83 @@ app.get('/remove/:path', function(req, res) {
 			res.send({numRemoved:numRemoved});
 		});
 	});
+});
 
+app.get(/^\/https?:\/\/.+$/, function(req, res) {
+	var u2 = req.url.slice(1),
+		u = url.parse(u2);
+
+	filesDb.find({url:u2}, function(err, docs) {
+		if (docs.length) {
+			var file = docs[0];
+
+			var path = __dirname+'/uploads/'+file.hash+'.'+file.extension;
+			res.sendfile(path);
+		} else {
+			(u.protocol === 'https:' ? https : http).get(u, function(httpres) {
+			res.status(httpres.statusCode);
+			console.log(res);
+
+			var type = httpres.headers['content-type'];
+			res.type(type);
+
+			var extension = mime.extension(type);
+
+			var temppath = temp.path({suffix: '.'+extension});
+			console.log(temppath);
+
+			var file = fs.createWriteStream(temppath);
+
+			var hash = crypto.createHash('sha1');
+			var size = 0;
+			httpres.on('data', function(data){
+				hash.update(data);
+				size += data.length;
+			});
+
+			httpres.pipe(file);
+			httpres.pipe(res);	
+			httpres.on('end', function() {
+
+				hash = hash.digest('hex');
+				var path = __dirname+'/uploads/'+hash+'.'+extension;
+
+				fs.exists(path, function(exists) {
+					if (exists) {
+						// Just remove the temporary file, we don't need it
+						fs.unlink(temppath);
+						console.log("remove", temppath);
+					} else {
+						fs.rename(temppath, path, function() {
+							// filesDb.insert({
+							console.log("move", temppath, path);
+							// 	size: f.size,
+							// 	hash: f.hash,
+							// 	extension: extension,
+							// 	type: f.type,
+							// 	name: f.name,
+							// 	mtime: f.lastModifiedDate
+							// });
+					
+							// res.send({name: f.name, status: 'ok'});
+						});
+					}
+					filesDb.insert({
+						size: size,
+						hash: hash,
+						extension: extension,
+						type: type,
+						name: u2.match(/[^\/]*$/)[0],
+						url: u2, 
+						mtime: new Date()
+					});
+				});
+			});
+		}).on('error', function(e) {
+			res.send(404, e.message);
+		});
+		}
+	});
 
 });
 

6eb9cc0ea1001aa80afa66c1b811d00e881b326a
Antoine Pultier <antoine.pultier@sintef.no>
2014-04-23 10:23:34 +0200

    Access-Control-Allow-Origin: *

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ var app = express();
 
 app.use(express.compress())
 app.use(express.logger());
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 

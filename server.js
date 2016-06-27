8a28e5db402fed5ff69c3ee466d89f79428c8530
Antoine Pultier <antoine.pultier@sintef.no>
2016-06-27 10:52:52 +0200

    Cleaning function syntax

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ var filesDb = new Nedb({filename: datapath+'files.db', autoload:true}),
 filesDb.ensureIndex({ fieldName: 'url', unique: true, sparse: true });
 filesDb.ensureIndex({ fieldName: 'mtime', unique: false, sparse: true });
 
-var gmWorker = async.queue(function(task, callback) {
+var gmWorker = async.queue((task, callback) => {
 	task(callback);
 }, process.env.CARACAL_CONCURRENCY || config.concurrency);
 
-var ffmpegWorker = async.queue(function(task, callback) {
+var ffmpegWorker = async.queue((task, callback) => {
 	task(callback);
 }, Math.min(Math.round((process.env.CARACAL_CONCURRENCY || config.concurrency)/4), 1));
 
@@ -42,7 +42,7 @@ var app = express();
 app.use(compression());
 app.use(morgan('short'));
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
@@ -51,13 +51,13 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.resolve(uploadDatapath)));
 
-app.get('/files', function(req, res) {
-	filesDb.find({}).sort({mtime: -1}).exec(function(err, docs) {
+app.get('/files', (req, res) => {
+	filesDb.find({}).sort({mtime: -1}).exec((err, docs) => {
 		res.send(docs);
 	});
 });
 
-app.get('/paginateFiles/:page', function(req, res) {
+app.get('/paginateFiles/:page', (req, res) => {
 	var pageSize = req.query.hasOwnProperty('pageSize') ?
 			Math.max(2, (parseInt(req.query.pageSize) || 0)) : 10,
 		page = Math.max(-1, (parseInt(req.params.page) || 0));
@@ -74,8 +74,8 @@ app.get('/paginateFiles/:page', function(req, res) {
 		req.limit(pageSize);
 	}
 
-	req.exec(function(err, docs) {
-		var req = filesDb.count({}, function(err, count) {
+	req.exec((err, docs) => {
+		var req = filesDb.count({}, (err, count) => {
 			res.json({
 				files: docs,
 				count: count
@@ -85,7 +85,7 @@ app.get('/paginateFiles/:page', function(req, res) {
 
 });
 
-app.get(/^\/resize\/(deform\/)?(\d+)\/(\d+)\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
+app.get(/^\/resize\/(deform\/)?(\d+)\/(\d+)\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, (req, res) => {
 	var path = req.params[3],
 		width = parseInt(req.params[1]),
 		height = parseInt(req.params[2]),
@@ -94,18 +94,18 @@ app.get(/^\/resize\/(deform\/)?(\d+)\/(\d+)\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, 
 	sendResizedImage(path, width, height, deform, res);
 });
 
-app.get(/^\/thumbnail\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
+app.get(/^\/thumbnail\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, (req, res) => {
 	var path = req.params[0];
 	sendThumbnail(path, res);
 });
 
-app.post('/upload', function(req, res) {
+app.post('/upload', (req, res) => {
 	var form = new formidable.IncomingForm();
 	form.uploadDir = uploadDatapath;
 	form.keepExtensions = true;
 	form.hash = 'sha1';
 
-	form.parse(req, function(err, fields, files) {
+	form.parse(req, (err, fields, files) => {
 		if (err || !files) {
 			console.log("File upload error", err);
 			res.status(500).send("Sorry, file upload error");
@@ -120,13 +120,13 @@ app.post('/upload', function(req, res) {
 
 			var path = uploadDatapath+"/"+f.hash+"."+extension;
 
-			fs.exists(path, function(exists) {
+			fs.exists(path, (exists) => {
 				if (exists) {
 					fs.unlink(f.path);
 					res.send({name: f.name, status: 'exists', hash: f.hash, extension: extension});
 				} else {
 
-					fs.rename(f.path, path, function() {
+					fs.rename(f.path, path, () => {
 						filesDb.insert({
 							size: f.size,
 							hash: f.hash,
@@ -134,7 +134,7 @@ app.post('/upload', function(req, res) {
 							type: f.type,
 							name: f.name,
 							mtime: f.lastModifiedDate
-						}, function() {
+						}, () => {
 							res.send({name: f.name, status: 'ok', hash: f.hash, extension: extension});
 						});
 				
@@ -148,7 +148,7 @@ app.post('/upload', function(req, res) {
 	})
 });
 
-app.get(/^\/remove\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
+app.get(/^\/remove\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, (req, res) => {
 
 	var deletionsKey = process.env.CARACAL_DELETIONS_KEY || config['deletions-key'];
 	if (deletionsKey && deletionsKey !== req.query.key) {
@@ -166,19 +166,19 @@ app.get(/^\/remove\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
 	var hash = path.slice(0, 40),
 		extension = path.slice(41);
 
-	picturesSizeDb.find({path: path}, function(err, docs) {
+	picturesSizeDb.find({path: path}, (err, docs) => {
 		if (err) {
 			res.status(500).send(err);
 			return;
 		}
 	
-		docs.forEach(function(doc) {
+		docs.forEach((doc) => {
 			fs.unlink(doc.unlink);
 		});
 
 		picturesSizeDb.remove({path: path});
 		
-		filesDb.remove({hash: hash, extension:extension}, {}, function(err, numRemoved) {
+		filesDb.remove({hash: hash, extension:extension}, {}, (err, numRemoved) => {
 			if (err) {
 				res.status(500).send(err);
 				return;
@@ -190,9 +190,9 @@ app.get(/^\/remove\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
 });
 
 function createThumbnail(path, uploadPath, thumbnailPath, res) {
-	gmWorker.push(function(callback){
+	gmWorker.push((callback) => {
 		gm(uploadPath).autoOrient().thumb(128,128,thumbnailPath, 90, callback);
-	}, function(err) {
+	}, (err) => {
 		if (err) {
 			res.redirect('/broken_thumbnail.png');
 			console.log(err);
@@ -214,24 +214,24 @@ function sendThumbnail(path, res) {
 		thumbnailPath += '.png';
 	}
 
-	fs.exists(thumbnailPath, function(exists) {
+	fs.exists(thumbnailPath, (exists) => {
 		if (exists) {
 			res.header('Cache-Control', config.cache);
 			res.sendFile(thumbnailPath, {root: __dirname});
 		} else {
-			fs.exists(uploadPath, function(exists) {
+			fs.exists(uploadPath, (exists) => {
 				if (!exists) {
 					res.status(404).send("File not found, sorry");
 					return;
 				}
 
 				if (isVideo) {
-					ffmpegWorker.push(function(callback){
-						ffmpeg(uploadPath).on('error', function(err,stdout,stderr) {
+					ffmpegWorker.push((callback) => {
+						ffmpeg(uploadPath).on('error', (err,stdout,stderr) => {
 							console.log(err);
 							res.redirect('/broken_thumbnail.png');
 							callback();
-						}).on('end', function() {
+						}).on('end', () => {
 							var ffmpegPath = uploadDatapath+'/ffmpeg-1-'+path+'.png'
 							picturesSizeDb.insert({unlink: ffmpegPath, path:path});
 							callback();
@@ -260,21 +260,21 @@ function sendResizedImage(path, width, height, deform, res) {
 		fullResizedPath = uploadDatapath+resizedPath,
 		uploadPath = uploadDatapath+"/"+path;
 
-	fs.exists(fullResizedPath, function(exists) {
+	fs.exists(fullResizedPath, (exists) => {
 		if (exists) {
 			res.header('Cache-Control', config.cache);
 			res.sendFile(fullResizedPath, {root: __dirname});
 		} else {
-			fs.exists(uploadPath, function(exists) {
+			fs.exists(uploadPath, (exists) => {
 				if (!exists) {
 					res.status(404).send("File not found, sorry");
 					return;
 				}
 
-				gmWorker.push(function(callback){
+				gmWorker.push((callback) => {
 					gm(uploadPath).autoOrient().resize(width,height, deform ? '!>' : '>')
 						.noProfile().write(fullResizedPath, callback);
-				}, function(err) {
+				}, (err) => {
 						if (err) {
 							console.log(err);
 							res.redirect('/broken_thumbnail.png');
@@ -297,12 +297,12 @@ function sendConvertedVideo(path, format, size, res) {
 		fullConvertedPath = uploadDatapath+"/"+convertedPath,
 		uploadPath = uploadDatapath+"/"+path;
 
-	fs.exists(fullConvertedPath, function(exists) {
+	fs.exists(fullConvertedPath, (exists) => {
 		if (exists) {
 			res.header('Cache-Control', config.cache);
 			res.sendFile(fullConvertedPath, {root: __dirname});
 		} else {
-			fs.exists(uploadPath, function(exists) {
+			fs.exists(uploadPath, (exists) => {
 				if (!exists) {
 					res.status(404).send("File not found, sorry");
 					return;
@@ -311,7 +311,7 @@ function sendConvertedVideo(path, format, size, res) {
 
 				console.log("The file "+path+" will be converted to "+format);
 
-				ffmpegWorker.push(function(callback){
+				ffmpegWorker.push((callback) => {
 					var proc = ffmpeg(uploadPath);
 
 					if (format === 'mp4') {
@@ -331,17 +331,17 @@ function sendConvertedVideo(path, format, size, res) {
 						.audioChannels(2)
 						.audioFrequency(44100)
 						.audioBitrate('192k')*/
-						.on('end', function() {
+						.on('end', () => {
 							console.log("The file "+path+" has been converted succesfully.");
 							picturesSizeDb.insert({unlink: fullConvertedPath, path:path});
 							res.header('Cache-Control', config.cache);
 							res.sendFile(fullConvertedPath, {root: __dirname});
 							callback();
 						})
-						.on('error', function(err) {
+						.on('error', (err) => {
 							console.log('An error happened: '+err.message);
 							res.status(500).send(err.message);
-							fs.exists(fullConvertedPath, function(exists) {
+							fs.exists(fullConvertedPath, (exists) => {
 								if (exists) {
 									fs.unlink(fullConvertedPath);	
 								}
@@ -363,7 +363,7 @@ function fetchDistantFile(u2, res, callback, reserror) {
 	// It's maybe the same for some other softwares
 	u2 = u2.replace(/^http(s?):\/([^\/])/,'http$1://$2');
 
-	filesDb.find({url:u2}, function(err, docs) {
+	filesDb.find({url:u2}, (err, docs) => {
 		if (docs.length) {
 			var file = docs[0];
 
@@ -390,7 +390,7 @@ function fetchDistantFile(u2, res, callback, reserror) {
 				'Accept-Language': 'fr,en-US;q=0.8,en;q=0.6'
 			};
 
-			(u.protocol === 'https:' ? https : http).get(u, function(httpres) {
+			(u.protocol === 'https:' ? https : http).get(u, (httpres) => {
 			
 				var type = httpres.headers['content-type'];
 
@@ -410,18 +410,18 @@ function fetchDistantFile(u2, res, callback, reserror) {
 
 				var hash = crypto.createHash('sha1');
 				var size = 0;
-				httpres.on('data', function(data){
+				httpres.on('data', (data) => {
 					hash.update(data);
 					size += data.length;
 				});
 
 				httpres.pipe(file);
-				httpres.on('end', function() {
+				httpres.on('end', () => {
 
 					hash = hash.digest('hex');
 					var path = uploadDatapath+'/'+hash+'.'+extension;
 
-					fs.exists(path, function(exists) {
+					fs.exists(path, (exists) => {
 						if (exists) {
 							// Just remove the temporary file, we don't need it
 							fs.unlink(temppath);
@@ -443,13 +443,13 @@ function fetchDistantFile(u2, res, callback, reserror) {
 						name: name,
 						url: u2, 
 						mtime: new Date()
-					}, function() {
+					}, () => {
 						if (callback) {
 							callback(path, hash, extension);
 						}
 					});
 				});
-			}).on('error', function(e) {
+			}).on('error', (e) => {
 				if (res) {
 					res.status(404).send(e.message);
 				} else if (reserror) {
@@ -460,36 +460,36 @@ function fetchDistantFile(u2, res, callback, reserror) {
 	});
 }
 
-app.get(/^\/https?:\/\/?.+$/, function(req, res) {
+app.get(/^\/https?:\/\/?.+$/, (req, res) => {
 	fetchDistantFile(req.url.slice(1), res);
 });
 
-app.get(/^\/fetch\/https?:\/\/?.+$/, function(req, res) {
-	fetchDistantFile(req.url.slice(7), false, function(filepath, hash, extension) {
+app.get(/^\/fetch\/https?:\/\/?.+$/, (req, res) => {
+	fetchDistantFile(req.url.slice(7), false, (filepath, hash, extension) => {
 		res.send({status: "ok", hash: hash, extension: extension});
 	}, res);
 });
 
-app.get(/^\/thumbnail\/https?:\/\/?.+$/, function(req, res) {
+app.get(/^\/thumbnail\/https?:\/\/?.+$/, (req, res) => {
 	var path = req.url.slice(11);
 
-	fetchDistantFile(path, false, function(filepath, hash, extension) {
+	fetchDistantFile(path, false, (filepath, hash, extension) => {
 		sendThumbnail(hash+"."+extension, res);
 	}, res);
 });
 
-app.get(/^\/resize\/(deform\/)?(\d+)\/(\d+)\/https?:\/\/?.+$/, function(req, res) {
+app.get(/^\/resize\/(deform\/)?(\d+)\/(\d+)\/https?:\/\/?.+$/, (req, res) => {
 	var path = req.url.match(/^\/resize\/(deform\/)?\d+\/\d+\/(https?:\/\/?.+)$/)[2],
 		width = parseInt(req.params[1]),
 		height = parseInt(req.params[2]),
 		deform = !!req.params[0];
 
-	fetchDistantFile(path, false, function(filepath, hash, extension) {
+	fetchDistantFile(path, false, (filepath, hash, extension) => {
 		sendResizedImage(hash+"."+extension, width, height, deform, res);
 	}, res);
 });
 
-app.get(/^\/convert\/(mp4|webm)\/(1080|720|480|240)\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, function(req, res) {
+app.get(/^\/convert\/(mp4|webm)\/(1080|720|480|240)\/([a-fA-F0-9]{40}\.[a-zA-Z0-9]+)$/, (req, res) => {
 	var path = req.params[2],
 		format = req.params[0];
 		size = parseInt(req.params[1]);
@@ -497,17 +497,17 @@ app.get(/^\/convert\/(mp4|webm)\/(1080|720|480|240)\/([a-fA-F0-9]{40}\.[a-zA-Z0-
 	sendConvertedVideo(path, format, size, res);
 });
 
-app.get(/^\/convert\/(mp4|webm)\/(1080|720|480|240)\/(https?:\/\/?.+)$/, function(req, res) {
+app.get(/^\/convert\/(mp4|webm)\/(1080|720|480|240)\/(https?:\/\/?.+)$/, (req, res) => {
 	var path = req.params[2],
 		format = req.params[0],
 		size = parseInt(req.params[1]);
 
-	fetchDistantFile(path, false, function(filepath, hash, extension) {
+	fetchDistantFile(path, false, (filepath, hash, extension) => {
 		sendConvertedVideo(hash+"."+extension, format, size, res);
 	}, res);
 });
 
 var serverPort = process.env.CARACAL_PORT || config.port;
-var server = app.listen(serverPort, function() {
+var server = app.listen(serverPort, () => {
 	console.log("Server started on http://localhost:"+serverPort+"/");
 });
